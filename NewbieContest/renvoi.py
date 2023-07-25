0f8598bb34cc8ef3aae3e16305c4eee34f07937c
nair0lf32 <florianedem@gmail.com>
2023-07-25 22:11:56 +0100

    solve renvoi on newbiecontest

---
+++ NewbieContest/renvoi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import requests # I feel like using requests today
+
+chall_url = "https://www.newbiecontest.org/epreuves/prog/prog1.php"
+ans_url = "https://www.newbiecontest.org/epreuves/prog/verifpr1.php?solution="
+
+with requests.Session() as s:
+	#ThiS IS THE CHALLENGE
+	myCookies = {"PHPSESSID":"GET",
+	"SMFCookie89":"YOUR OWN",
+	"admin":"COOKIES"} 
+
+	chall = s.get(chall_url, cookies=myCookies)
+	response = chall.text
+	print(response)
+	num = response.split(":")[-1].strip()
+	print(s.get(ans_url + num, cookies=myCookies).text)
