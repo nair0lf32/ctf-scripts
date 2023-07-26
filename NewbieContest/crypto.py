ef838e89200a535b13da973f41dd2a79c78873f2
nair0lf32 <florianedem@gmail.com>
2023-07-26 22:59:08 +0100

    solve crypto on newbiecontest

---
+++ NewbieContest/crypto.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import requests
+
+chall_url = "https://www.newbiecontest.org/epreuves/prog/prog5.php"
+ans_url = "https://www.newbiecontest.org/epreuves/prog/verifpr5.php?solution="
+
+# Reinventing the emperor's wheel
+def decipher(encrypted, shift):
+    start = ord('a')
+    decrypted = ""
+    for char in encrypted:
+        decrypted += chr(((ord(char) - start - shift) % 26) + start)
+    return decrypted
+
+with requests.Session() as s:
+	myCookies = {"MY-PRECIOUS-COOKIES"} 
+
+	chall = s.get(chall_url, cookies=myCookies)
+	exp = chall.content.decode("ascii").split(" ")
+	crypt = exp[6].replace('\'','')
+	key = int(exp[-1].replace('\'',''))
+	print("crypted: ", crypt, " key: ", key)
+	solution = decipher(crypt, key) #MINUSKEY
+	print("decoded: ", solution)
+	print(s.get(ans_url + str(solution), cookies=myCookies).text)
+	
